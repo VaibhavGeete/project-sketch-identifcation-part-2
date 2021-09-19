4a1cd0400a9f502a97eedbd238b73326ed83a4b9
vaibhav <80770886+VaibhavGeete@users.noreply.github.com>
2021-09-19 12:03:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+array_1=['pen','paper','book','bottle'];
+random_number=math.floor((math.random()*array_1.length)+1);
+console.log(array_1);
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas()
+{
+    background("white");
+}
+
+function preload()
+{
+
+}
+
+function draw()
+{
+    
+}
